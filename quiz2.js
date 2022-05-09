42576723e12663525a4c48289063fab429295c99
VKhushi07 <94143199+VKhushi07@users.noreply.github.com>
2022-05-09 19:25:19 -0400

    Final upload

---
+++ quiz2.js
@@ -0,0 +1,480 @@
+var quiz = 
+{
+  // (A) PROPERTIES
+  // (A1) QUESTIONS & ANSWERS
+  // Q = QUESTION, O = OPTIONS, A = CORRECT ANSWER
+  data: [
+   {
+    q : "41. The Trespass To Property Act is",
+    o : [
+      "Federal",
+      "Municipal",
+      "Provincial",
+      "None of the above"
+    ],
+    a : 2
+  },
+   {
+    q : "42. A landlord can enter a tenants unit with 24 hours notice however the landlord is limited to specific times of entry",
+    o : [
+      "7 am - 10 am",
+      "8 am - 6 pm",
+      "7 am - 9 pm",
+      "8 am - 8 pm"
+    ],
+    a : 3
+  },
+   {
+    q : "43. With a Security license which of the following positions would not be covered under the PSISA",
+    o : [
+      "Bouncer",
+      "Loss prevention",
+      "Security guard",
+      "Bodyguard",
+      "bailiff"
+    ],
+    a : 4
+  },
+   {
+    q : "44. Class C Fires Involve",
+    o : [
+      "Flammable Liquids",
+      "Poisonous and Infectious Material",
+      "Ordinary Combustibles",
+      "Electronic Equipment"
+    ],
+    a : 3
+  },
+  {
+    q : "45. What would NOT be an example of Stereotyping",
+    o : [
+      "All homeless people are dangerous",
+      "Kids in school on the lunch program are poor",
+      "People that get into financial trouble do so because they are irresponsible people",
+      "People that drive over the speed limit are more likely to cause an accident"
+    ],
+    a : 3
+  },
+   {
+    q : "46. Class D Fires Involve",
+    o : [
+      "Combustible Metals",
+      "Flammable Liquids",
+      "Dangerously Reactive Material",
+      "None of the above"
+    ],
+    a : 0
+  },
+   {
+    q : "47. A property owner, occupier or agent of a property owner may arrest without warrant found committing a criminal offence",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 0
+  },
+   {
+    q : "48. Para-verbal communication and body language play a very minor role in communication",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 1
+  },
+   {
+    q : "49. A Justice Of The Peace is referred to as",
+    o : [
+      "Your Greatness",
+      "Your Worship",
+      "Your Honor",
+      "Your Worthiness"
+    ],
+    a : 1
+  },
+   {
+    q : "50. What is Documentary evidence",
+    o : [
+      "Phone records",
+      "any document which is presented and allowed as evidence",
+      "An NSF cheque",
+      "All of the above"
+    ],
+    a : 3
+  },
+   {
+    q : "51. A police officer, or the occupier of a premises, or a person authorized by the occupier may arrest without warrant any person he or she believes on reasonable and probable grounds to be on the premises in contravention of section 2 of the Trespass To Property Act",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 0
+  },
+   {
+    q : "52. What are post standing orders?",
+    o : [
+      "Written orders that set out all emergency procedures",
+      "written rules which may include but are not limited to operational, procedural and administrative matters",
+      "A list of things to do before you start your shift as a Security guard",
+      "They are a set of orders included in the emergency measures pla"
+    ],
+    a : 1
+  },
+   {
+    q : "53. A class B fire involves",
+    o : [
+      "Vegetable oils or fats",
+      "Ordinary combustible materials",
+      "Electrical equipment",
+      "Flammable liquids"
+    ],
+    a : 3
+  },
+   {
+    q : "54. Which of the following would NOT be an example of Use of Force during an arrest",
+    o : [
+      "Placing a suspect in a choke hold",
+      "Slapping a non-compliant suspect in the head to get his attention",
+      "Pushing a suspect because he is not moving fast enough",
+      "None of the above"
+    ],
+    a : 3
+  },
+  {
+    q : "55. 10 code for requesting an ambulance",
+    o : [
+      "10-67",
+      "10-4",
+      "10-10",
+      "10-52"
+    ],
+    a : 3
+  },
+   {
+    q : "56. What Provincial Law in Ontario gives everyone equal rights and opportunities",
+    o : [
+      "Human Rights Code",
+      "Trespass To Property Act",
+      "Charter of Rights",
+      "Criminal Code Of Canada"
+    ],
+    a : 0
+  },
+   {
+    q : "57. What does this symbol represent",
+    o : [
+      "Flammable liquids",
+      "Corrosive Material",
+      "Compressed Gas",
+      "Oxidizing Materials"
+    ],
+    a : 2
+  },
+   {
+    q : "58. Under what section of the Charter Of Rights gives a person the right to join an organization",
+    o : [
+      "Section 10 (a)",
+      "Section 2 (d)",
+      "Section 494(1)",
+      "Section 9"
+    ],
+    a : 1
+  },
+   {
+    q : "59. Section 26 of the Criminal Code deals with",
+    o : [
+      "Trespass",
+      "Theft",
+      "Excessive force",
+      "Breach of the peace"
+    ],
+    a : 2
+  },
+   {
+    q : "60. As a Security guard if you are rude to members of the public you are in violation of",
+    o : [
+      "PSISA Code Of Conduct",
+      "Police Act",
+      "Human Rights Code",
+      "Criminal Code"
+    ],
+    a : 0
+  },
+   {
+    q : "61. As a Security guard you have been told to stand near and watch a truck load of computers being unloaded to an office. You are dressed in plain clothes. The purpose of doing this is to make sure that no one steals computers when the movers are inside the office. What kind of surveillance would you be conducting",
+    o : [
+      "Overt Surveillance",
+      "Covert Surveillance",
+      "Tandem Surveillance",
+      "None of the above"
+    ],
+    a : 0
+  },
+   {
+    q : "62. The highest court in Canada is called",
+    o : [
+      "Court of Queen's bench",
+      "Supreme Court of Canada",
+      "Superior Court",
+      "None of the above"
+    ],
+    a : 1
+  },
+   {
+    q : "63. PIPEDA Personal Information Protection and Electronic Documents Act",
+    o : [
+      "Protects Personal information",
+      "Protects sensitive information",
+      "Is Federal legislation",
+      "All of the above"
+    ],
+    a : 3
+  },
+   {
+    q : "64. What section of the criminal code protects a private person making an lawful arrest",
+    o : [
+      "Sec 26",
+      "Sec 25",
+      "Sec 27",
+      "None of the above"
+    ],
+    a : 1
+  },
+  {
+    q : "65. Toronto Metro Police are",
+    o : [
+      "Federal Police",
+      "Provincial Police",
+      "Municipal Police",
+      "Regional Police"
+    ],
+    a : 2
+  },
+   {
+    q : "66. A security guard is a person who performs work, for remuneration, that consists primarily of guarding or patrolling for the purpose of protecting persons or property",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 0
+  },
+   {
+    q : "67. Fingerprints, hair, cosmetics, plant fibers, blood, mineral fibers are considered what type of evidence in",
+    o : [
+      "Conscripted evidence",
+      "Trace evidence",
+      "Hearsay evidence",
+      "None of the above"
+    ],
+    a : 1
+  },
+   {
+    q : "68. A Hybrid offence is a criminal offence that has the option of being indictable or summary",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 0
+  },
+   {
+    q : "69. Under the Residential Tenancies Act, during a tenants occupancy a landlord cannot alter or change the locking mechanism to the unit without",
+    o : [
+      "Giving the tenant replacement keys",
+      "Allowing the tenant the option of buying his own lock",
+      "Notifying the tenant at least 48 hours in advance",
+      "Compensating the tenant for loss of use"
+    ],
+    a : 0
+  },
+   {
+    q : "70. Everyone who is authorized by law to use force is criminally responsible for any excess thereof. What section of the Criminal Code deals with Excess of Force",
+    o : [
+      "Section 26 Criminal Code",
+      "Section 34 Criminal Code",
+      "Section 41 Criminal Code",
+      "None Of The Above"
+    ],
+    a : 0
+  },
+   {
+    q : "71. Which of the following is an exception to the hearsay rule",
+    o : [
+      "Ex-parte testimon",
+      "Dying declaration",
+      "Best evidence rule",
+      "All of the above"
+    ],
+    a : 1
+  },
+   {
+    q : "72. If convicted as a Security guard the maximum fine under the PSISA is",
+    o : [
+      "$2000",
+      "$10,000",
+      "$25,000",
+      "$350,000"
+    ],
+    a : 2
+  },
+   {
+    q : "73. When you make a mistake in your notebook how should you correct it?",
+    o : [
+      "Tear the page out and start over",
+      "Use a pen and scribble out the error until it can't be seen",
+      "Draw one line through the mistake and initial it",
+      "Get a new notebook from the company"
+    ],
+    a : 2
+  },
+   {
+    q : "74. What is the legal drinking age in Ontario",
+    o : [
+      "16",
+      "22",
+      "19",
+      "18"
+    ],
+    a : 2
+  },
+  {
+    q : "75. Every province and territory has privacy legislation governing the collection, use and disclosure of personal information held by government agencies",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 0
+  },
+   {
+    q : "76. Opinion evidence is given by court certified expert witnesses",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 0
+  },
+   {
+    q : "77. The offence of Theft under the Criminal Code is a Hybrid offence",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 0
+  },
+   {
+    q : "78. Who determines what evidence will or will not be admitted in court",
+    o : [
+      "Judge",
+      "Prosecutor",
+      "Defence Council",
+      "The accused"
+    ],
+    a : 0
+  },
+   {
+    q : "79. Using tactical communications can prevent situations where force may have to be used",
+    o : [
+       "True",
+       "False"
+    ],
+    a : 0
+  },
+   {
+    q : "80. A Summary Offence under the Criminal Code is considered a less serious criminal offence",
+    o : [
+       "True",
+       "False"
+    ],
+    a : 0
+  }
+  ],
+
+
+  // (A2) HTML ELEMENTS
+  hWrap: null, // HTML quiz container
+  hQn: null, // HTML question wrapper
+  hAns: null, // HTML answers wrapper
+
+  // (A3) GAME FLAGS
+  now: 0, // current question
+  score: 0, // current score
+
+  // (B) INIT QUIZ HTML
+  init: () => {
+    // (B1) WRAPPER
+    quiz.hWrap = document.getElementById("quizWrap");
+
+    // (B2) QUESTIONS SECTION
+    quiz.hQn = document.createElement("div");
+    quiz.hQn.id = "quizQn";
+    quiz.hWrap.appendChild(quiz.hQn);
+
+    // (B3) ANSWERS SECTION
+    quiz.hAns = document.createElement("div");
+    quiz.hAns.id = "quizAns";
+    quiz.hWrap.appendChild(quiz.hAns);
+
+    // (B4) GO!
+    quiz.draw();
+  },
+
+  // (C) DRAW QUESTION
+  draw: () => {
+    // (C1) QUESTION
+    quiz.hQn.innerHTML = quiz.data[quiz.now].q;
+    
+
+    // (C2) OPTIONS
+    quiz.hAns.innerHTML = "";
+    for (let i in quiz.data[quiz.now].o) {
+      let radio = document.createElement("input");
+      radio.type = "radio";
+      radio.name = "quiz";
+      radio.id = "quizo" + i;
+      quiz.hAns.appendChild(radio);
+      let label = document.createElement("label");
+      label.innerHTML = quiz.data[quiz.now].o[i];
+      label.setAttribute("for", "quizo" + i);
+      label.dataset.idx = i;
+      label.addEventListener("click", () => { quiz.select(label); });
+      quiz.hAns.appendChild(label);
+    }
+  },
+
+  // (D) OPTION SELECTED
+  select: (option) => {
+    // (D1) DETACH ALL ONCLICK
+    let all = quiz.hAns.getElementsByTagName("label");
+    for (let label of all) {
+      label.removeEventListener("click", quiz.select);
+    }
+
+    // (D2) CHECK IF CORRECT
+    let correct = option.dataset.idx == quiz.data[quiz.now].a;
+    if (correct) {
+      quiz.score++;
+      option.classList.add("correct");
+    } else {
+      option.classList.add("wrong");
+    }
+
+    // (D3) NEXT QUESTION OR END GAME
+    quiz.now++;
+    setTimeout(() => {
+      if (quiz.now < quiz.data.length) 
+      { 
+        quiz.draw(); 
+      }
+      else {
+        quiz.hQn.innerHTML = `You have answered ${quiz.score} of ${quiz.data.length} correctly.`;
+        quiz.hAns.innerHTML = "";
+      }
+    }, 1000);
+  },
+
+  // (E) RESTART QUIZ
+  reset : () => {
+    quiz.now = 0;
+    quiz.score = 0;
+    quiz.draw();
+  }
+};
+window.addEventListener("load", quiz.init);
